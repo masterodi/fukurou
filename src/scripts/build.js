215bc0a08cd267d47f561e1f8509ffcc17da1966
masterodi <ovidiud.dev@proton.me>
2025-12-06 13:23:19 +0200

    Init

---
+++ src/scripts/build.js
@@ -0,0 +1,24 @@
+import fs from "fs";
+import path from "path";
+import { generate } from "./generate.js";
+
+const __dirname = path.resolve();
+
+const THEMES_DIR = path.join(__dirname, "themes");
+
+if (!fs.existsSync(THEMES_DIR)) {
+  fs.mkdirSync(THEMES_DIR);
+}
+
+const build = async () => {
+  const { base } = await generate();
+
+  return Promise.all([
+    fs.promises.writeFile(
+      path.join(THEMES_DIR, "placeholder.json"),
+      JSON.stringify(base, null, 4)
+    ),
+  ]);
+};
+
+build();
---
+++ src/scripts/generate.js
@@ -0,0 +1,77 @@
+import { readFile } from "fs/promises";
+import { DEFAULT_SCHEMA, load, Type } from "js-yaml";
+import path from "path";
+import tinycolor from "tinycolor2";
+
+const __dirname = path.resolve();
+
+/**
+ * @typedef {Object} TokenColor - Textmate token color.
+ * @prop {string} [name] - Optional name.
+ * @prop {string[]} scope - Array of scopes.
+ * @prop {Record<'foreground'|'background'|'fontStyle',string|undefined>} settings - Textmate token settings.
+ *       Note: fontStyle is a space-separated list of any of `italic`, `bold`, `underline`.
+ */
+
+/**
+ * @typedef {Object} Theme - Parsed theme object.
+ * @prop {Record<'base'|'ansi'|'brightOther'|'other', string[]>} placeholder - Theme color variables.
+ * @prop {Record<string, string|null|undefined>} colors - VSCode color mapping.
+ * @prop {TokenColor[]} tokenColors - Textmate token colors.
+ */
+
+/**
+ * @typedef {(yamlObj: Theme) => Theme} ThemeTransform
+ */
+
+const withAlphaType = new Type("!alpha", {
+  kind: "sequence",
+  construct: ([hexRGB, alpha]) => hexRGB + alpha,
+  represent: ([hexRGB, alpha]) => hexRGB + alpha,
+});
+
+const schema = DEFAULT_SCHEMA.extend([withAlphaType]);
+
+/**
+ * Soft variant transform.
+ * @type {ThemeTransform}
+ */
+// eslint-disable-next-line no-unused-vars
+const transformSoft = (theme) => {
+  /** @type {Theme} */
+  const soft = JSON.parse(JSON.stringify(theme));
+  const brightColors = [...soft.dracula.ansi, ...soft.dracula.brightOther];
+  for (const key of Object.keys(soft.colors)) {
+    if (brightColors.includes(soft.colors[key])) {
+      soft.colors[key] = tinycolor(soft.colors[key])
+        .desaturate(20)
+        .toHexString();
+    }
+  }
+  soft.tokenColors = soft.tokenColors.map((value) => {
+    if (brightColors.includes(value.settings.foreground)) {
+      value.settings.foreground = tinycolor(value.settings.foreground)
+        .desaturate(20)
+        .toHexString();
+    }
+    return value;
+  });
+  return soft;
+};
+
+export const generate = async () => {
+  const yamlFile = await readFile(
+    path.join(__dirname, "src", "placeholder.yaml")
+  );
+
+  /** @type {Theme} */
+  const base = load(yamlFile, { schema });
+
+  for (const key of Object.keys(base.colors)) {
+    if (!base.colors[key]) {
+      delete base.colors[key];
+    }
+  }
+
+  return { base };
+};
